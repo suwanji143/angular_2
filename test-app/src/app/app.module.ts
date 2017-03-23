8b256a93037f7ba75c783ea162f55b7197ff6b32
suwanji <suwanji@gmail.com>
2017-03-23 09:01:02 +0530

    Added two test component using CLI

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherComponent,
+    AnotherComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
